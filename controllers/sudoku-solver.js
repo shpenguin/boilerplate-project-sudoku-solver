5dd4dd2bd1680d1c4ba2017c7d26c5f6d1b0d4d4
shpenguin <39072247+shpenguin@users.noreply.github.com>
2021-11-01 12:19:35 +0800

    update

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,16 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+
+    if (/[^1-9||.]/.test(puzzleString)) {
+      return { error: 'Invalid characters in puzzle' };
+    }
+
+    if (puzzleString.length !== 81) {
+      return { error: 'Expected puzzle to be 81 characters long' };
+    }
+
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
